fee3487e1a4e21731f13d67ffad31a3d05b2426b
[domalei] <[alei.dominic@gmail.com]>
2020-08-02 21:54:25 -0600

    initial commit

---
+++ debris.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 31 11:24:10 2020
+
+@author: leafuser
+"""
+
+
+#axSlider1 = plt.axes([0.1, 0.2, 0.8, 0.05])
+#slider1 = Slider(axSlider1, 'Slider1', valmin=0, valmax=5)
+'''Add in the cordinates of the nodes and then refer to them rather than hard coding'''
+
+#random generation
+
+'''iterations =[]
+for i in range(28):
+    nums = random.uniform(0,3)
+    iterations.append(nums)'''
+
+
+#print(iterations)
+
+
+
+#slider = Slider('Force', valmin=0, valmax=100)
+#N takes out the info from details. n[0] incourperates all of the nodes and n[1] incourperates all of the segments. Therefore n[0][0] gives the first node independtly. 
